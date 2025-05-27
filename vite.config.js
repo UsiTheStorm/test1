a8d1f4309cf38ede00d86eb730f3991b597c33e9
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-05-27 12:01:26 +0100

    Update deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const isGitHub = process.env.DEPLOY_TARGET === 'GH';
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/test1/',
+    // base: '/test1/',
+    base: isGitHub ? '/test1/' : '/',
     build: {
         sourcemap: true,
         emptyOutDir: true,
